0282d8269a2b5f467f07617fc64d08620614cfb3
Sam <wafulasammie@gmail.com>
2024-12-10 15:32:13 +0300

    hot fixes

--- frontend/src/components/UsersTable.tsx
+++ frontend/src/components/UsersTable.tsx
@@ -15,6 +15,28 @@ export default function UsersTable (){
             {loading && 'loading...'}
             {error && 'error!'}
             {JSON.stringify(users)}
+            <div className="overflow-x-auto bg-white shadow-lg rounded-lg">
+                <table className="min-w-full table-auto text-sm">
+                    <thead className="bg-gray-200">
+                    <tr>
+                        <th className="py-2 px-4 text-left">Name</th>
+                        <th className="py-2 px-4 text-left">Email</th>
+                        <th className="py-2 px-4 text-left">Phone</th>
+                        <th className="py-2 px-4 text-left">Role</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {users.map((user:any) => (
+                        <tr key={user.id} className={users.indexOf(user) % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
+                        <td className="py-2 px-4">{user.name}</td>
+                        <td className="py-2 px-4">{user.email}</td>
+                        <td className="py-2 px-4">{user.phone}</td>
+                        <td className="py-2 px-4">{user.role}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                </div>
         </div>
     )
 }
